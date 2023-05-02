1e0df9397b6192f23f955c6d931b80b9a4cd9b98
TheRival2002 <124076661+TheRival2002@users.noreply.github.com>
2023-05-02 10:22:53 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const mobileNav = document.querySelector(".mobile-nav");
 const headerNav = document.querySelector(".header-nav");
+const choice = document.querySelector(".choice-benefit");
+const choiceTitle = document.querySelector(".choice-title");
 
 const header = document.querySelector(".header");
-const logoInter = document.querySelector(".logo-inter");
 const logoImg = document.querySelector(".logo-img");
-const optionsCrop = {
-  rootMargin: "-220px 0px 0px 0px",
-};
+
+const headerBtns = document.querySelectorAll(".header-btn");
 
 mobileNav.addEventListener("click", function () {
   headerNav.hasAttribute("data-visible")
@@ -15,20 +15,58 @@ mobileNav.addEventListener("click", function () {
   headerNav.toggleAttribute("data-visible");
 });
 
-const logoImgObserver = new IntersectionObserver(function (
-  entries,
-  logoImgObserver
-) {
-  entries.forEach((entry) => {
-    if (!entry.isIntersecting) {
-      logoImg.classList.add("logo-img-crop");
-      header.classList.add("header-scroll");
-    } else {
-      logoImg.classList.remove("logo-img-crop");
-      header.classList.remove("header-scroll");
-    }
-  });
-},
-optionsCrop);
+window.addEventListener("scroll", () => {
+  const choiceFromTop = choiceTitle.getBoundingClientRect().top;
+  const headerHeight = header.getBoundingClientRect().height;
+  const offset = window.pageYOffset;
+  if (offset > choiceFromTop - 50) {
+    choice.classList.add("animate");
+  } else if (offset < choiceFromTop - 300) {
+    choice.classList.remove("animate");
+  }
 
-logoImgObserver.observe(logoInter);
+  if (offset > headerHeight + 50) {
+    header.classList.add("header-scroll");
+    logoImg.classList.add("logo-img-crop");
+  } else {
+    header.classList.remove("header-scroll");
+    logoImg.classList.remove("logo-img-crop");
+  }
+});
+
+let counter = 0;
+
+headerBtns.forEach((headerBtn) => {
+  let attr = headerBtn.dataset.attr;
+  headerBtn.addEventListener("click", (btn) => {
+    const span = headerBtn.querySelector("span");
+    counter++;
+    if (counter % 2 === 1) {
+      span.classList.add("toggle-dark");
+      attr = "dark";
+    } else {
+      span.classList.remove("toggle-dark");
+      attr = "light";
+    }
+    document.body.classList.toggle("dark-mode");
+    localStorage.setItem("attr", attr);
+  });
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+  const spans = document.querySelectorAll(".header-btn span");
+  let getAttr = localStorage.getItem("attr");
+  if (getAttr === "dark") {
+    document.body.classList.add("dark-mode");
+    spans.forEach((span) => {
+      span.classList.add("toggle-dark");
+    });
+    counter = 1;
+  } else {
+    document.body.classList.remove("dark-mode");
+    spans.forEach((span) => {
+      span.classList.remove("toggle-dark");
+    });
+    counter = 0;
+  }
+});
