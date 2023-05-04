d29ccb42863cb52a37cc3d591a61bb4309aab22c
TheRival2002 <124076661+TheRival2002@users.noreply.github.com>
2023-05-04 11:59:53 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const mobileNav = document.querySelector(".mobile-nav");
 const headerNav = document.querySelector(".header-nav");
 const choice = document.querySelector(".choice-benefit");
 const choiceTitle = document.querySelector(".choice-title");
+const benefits = document.querySelectorAll(".benefit");
 
 const header = document.querySelector(".header");
 const logoImg = document.querySelector(".logo-img");
@@ -15,19 +16,37 @@ mobileNav.addEventListener("click", function () {
   headerNav.toggleAttribute("data-visible");
 });
 
+animateOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px -200px 0px",
+};
+
+const animateObserver = new IntersectionObserver((entries, e) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      entry.target.classList.remove("animate");
+      return;
+    } else {
+      entry.target.classList.add("animate");
+      animateObserver.unobserve(entry.target);
+    }
+  });
+}, animateOptions);
+
 window.addEventListener("scroll", () => {
-  const choiceFromTop = choiceTitle.getBoundingClientRect().top;
   const headerHeight = header.getBoundingClientRect().height;
   const offset = window.pageYOffset;
-  if (offset > choiceFromTop - 50) {
-    choice.classList.add("animate");
-  } else if (offset < choiceFromTop - 300) {
-    choice.classList.remove("animate");
+  if (offset < 200) {
+    benefits.forEach((benefit) => {
+      animateObserver.observe(benefit);
+    });
   }
 
   if (offset > headerHeight + 50) {
     header.classList.add("header-scroll");
     logoImg.classList.add("logo-img-crop");
+    headerNav.removeAttribute("data-visible");
+    mobileNav.setAttribute("aria-expanded", false);
   } else {
     header.classList.remove("header-scroll");
     logoImg.classList.remove("logo-img-crop");
